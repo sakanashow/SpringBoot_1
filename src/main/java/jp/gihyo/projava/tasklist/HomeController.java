95de53bb5c6b1f7b30405e97eaa72e9c9a2496b3
sakana <mashou625@gmail.com>
2023-06-06 11:36:30 +0900

    完成版

--- src/main/java/jp/gihyo/projava/tasklist/HomeController.java
+++ src/main/java/jp/gihyo/projava/tasklist/HomeController.java
@@ -1,5 +1,7 @@
 package jp.gihyo.projava.tasklist;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.config.Task;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,7 @@ public class HomeController {
     private List<TaskItem> taskItems = new ArrayList<>();
     @GetMapping("/list")
     String listItems(Model model) {
+        List<TaskItem> taskItems = dao.findAll();
         model.addAttribute("taskList", taskItems);
         return  "home";
     }
@@ -25,7 +28,7 @@ public class HomeController {
     String addItem(@RequestParam("task") String task, @RequestParam("deadline") String deadline) {
         String id = UUID.randomUUID().toString().substring(0, 8);
         TaskItem item = new TaskItem(id, task, deadline, false);
-        taskItems.add(item);
+        dao.add(item);
         return "redirect:/list";
     }
 
@@ -33,5 +36,24 @@ public class HomeController {
      String hello(Model model) {
         model.addAttribute("time", LocalDateTime.now());
         return "hello";
+    }
+    @GetMapping("/delete")
+    String deleteItem(@RequestParam("id") String id) {
+        dao.delete(id);
+        return "redirect:list";
+    }
+
+    @GetMapping("/update")
+    String updateItem(@RequestParam("id") String id, @RequestParam("task") String task, @RequestParam("deadline") String deadline,
+    @RequestParam("done") boolean done)
+    {
+        TaskItem taskItem = new TaskItem(id, task, deadline, done);
+        dao.update(taskItem);
+        return "redirect:list";
+    }
+    private final TaskListDao dao;
+    @Autowired
+    HomeController(TaskListDao dao) {
+        this.dao = dao;
     }
 }
